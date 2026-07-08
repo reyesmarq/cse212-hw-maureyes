ff6192b9e337ab16116e3d1c2946386a55023c73
Mauricio Reyes <maureyes.dev@gmail.com>
2026-07-07 22:08:18 -0600

    complete week 2 prepare stacks assignment

--- week02/learn/Program.cs
+++ week02/learn/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("\n======================\nSimple Queue\n======================");
+﻿Console.WriteLine("\n======================\nSimple Stack\n======================");
+SimpleStack.Run();
+
+Console.WriteLine("\n======================\nSimple Queue\n======================");
 SimpleQueue.Run();
 
 // Uncomment and run as you get to the solution part
